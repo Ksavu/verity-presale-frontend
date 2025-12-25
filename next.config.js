c07c37f4041ea4a957ea4503a5a410fbbbffc8b9
Ksavu <nenadv36@gmail.com>
2025-12-25 16:00:45 +0100

    Fix AdminPanel import

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export", // obavezno za Netlify static site
+    reactStrictMode: true,
+  };
+  module.exports = nextConfig;
+  
--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,4 +1,4 @@
-import { AdminPanel } from "../components/AdminPanel";
+import AdminPanel from "../components/AdminPanel";
 
 export default function Admin() {
   return (
@@ -7,3 +7,4 @@ export default function Admin() {
     </div>
   );
 }
+
